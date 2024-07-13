c40ceb4ee1e46d6dad39df57f4e9e23e93f02a65
ijTuhin <isratishu0209@gmail.com>
2024-07-13 22:45:24 +0600

    react icon added for max-min

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,9 @@ import { useState } from "react";
 // import reactLogo from "./assets/react.svg";
 // import viteLogo from "/vite.svg";
 // import "./App.css";
-
+import { TbMaximize } from "react-icons/tb";
+import { LuMinimize } from "react-icons/lu";
+import { BsCaretRightFill } from "react-icons/bs";
 function App() {
   const [size, setSize] = useState(true);
 
@@ -10,27 +12,36 @@ function App() {
     <main className="bg-stone-950">
       <header className={`border-2 h-16`}></header>
       {/* ==================== Code Editor Section ==================== */}
-      <div className={`w-full ${size ? `mt-[4.35rem] h-[38rem]` : `h-full absolute top-0`}`}>
+      <div
+        className={`w-full ${
+          size ? `mt-[4.35rem] h-[38rem]` : `h-full absolute top-0`
+        }`}
+      >
         <div className={`flex justify-center h-full`}>
           <div className={`${size ? `w-[75rem]` : `w-full`} flex flex-col`}>
             {/* ==================== Code Editor Size Btn ==================== */}
-            <div className={`w-full flex justify-end bg-stone-300`}>
-              <button
-                type="button"
-                onClick={() => {
-                  setSize(!size);
-                  console.log(size);
-                }}
-                className={`p-1`}
-              >
-                O
-              </button>
+            <div className="grid grid-cols-2">
+              <div className={`w-full bg-stone-300`}></div>
+              <div className={`w-full flex justify-end bg-stone-300`}>
+                <button
+                  type="button"
+                  onClick={() => {
+                    setSize(!size);
+                    console.log(size);
+                  }}
+                  className={`py-1.5 px-2 text-gray-500`}
+                >
+                  {size ? <TbMaximize /> : <LuMinimize />}
+                </button>
+              </div>
             </div>
             {/* ==================== Code Editor Box ==================== */}
-            <section className={`bg-stone-500 shadow-3xl w-full flex-grow grid grid-cols-2 divide-x-2`}>
+            <section
+              className={`bg-stone-500 shadow-3xl w-full flex-grow grid grid-cols-2 divide-x-2`}
+            >
               {/* ==================== Code Editor ==================== */}
               <div className={`divide-y-2`}>
-                <header className={`px-2.5 py-1  flex flex-col`}>
+                <header className={`px-2.5 py-1`}>
                   <button type="button" className={`text-left`}>
                     x
                   </button>
@@ -39,9 +50,12 @@ function App() {
               </div>
               {/* ==================== Code Editor Console ==================== */}
               <div className={`divide-y-2`}>
-                <header className={`px-2.5 py-1  flex flex-col`}>
-                  <button type="button" className={`text-left`}>
+                <header className={`px-2.5 py-1`}>
+                  {/* <button type="button" className={`text-left`}>
                     x
+                  </button> */}
+                  <button type="button" className={`py-1.5 px-2 text-black`}>
+                    {<BsCaretRightFill />}
                   </button>
                 </header>
                 <div className="flex-grow"></div>
